d76d4d5b64d0164f98d843f4ffc05ce7f8b5ee89
Iris <leejing397@gmail.com>
2018-03-12 22:39:06 +0800

    新增三方库功能

---
+++ testSpec/GetSimilarity/GetSimilarity/Classes/Man.h
@@ -0,0 +1,13 @@
+//
+//  Man.h
+//  GetSimilarity
+//
+//  Created by Iris on 2018/3/12.
+//  Copyright © 2018年 Iris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Man : NSObject
+
+@end
---
+++ testSpec/GetSimilarity/GetSimilarity/Classes/PersonVC.h
@@ -0,0 +1,13 @@
+//
+//  PersonVC.h
+//  GetSimilarity
+//
+//  Created by Iris on 2018/3/12.
+//  Copyright © 2018年 Iris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonVC : UIViewController
+
+@end
